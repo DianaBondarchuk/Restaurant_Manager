eb4ceb44b3b757d13f38511345045d2be5677abf
Kobri_to00@student.itstep.org <Kobri_to00@student.itstep.org>
2025-11-14 14:00:51 +0200

    Buttton_Click

--- Restaurant_Manager/MainWindow.xaml.cs
+++ Restaurant_Manager/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ namespace RestaurantManager
         //public DependencyObject MainContent { get; }
         //RelayCommand command;
         private readonly AppDbContext _context;
+        private readonly UserService _userService;
         MenuViewModel menuViewModel;
         IMenuItemService menuService;
         public MainWindow()
@@ -31,33 +32,55 @@ namespace RestaurantManager
             //_navigationService.Navigate(new LoginView());
             //command = new RelayCommand((o) => Login());
             _context = new AppDbContext();
+            _userService = new UserService(_context);
             LoginView login =new LoginView( _context); 
             login.ShowDialog();
             menuService = new MenuItemService(_context);
             menuViewModel = new MenuViewModel(menuService);    
         }
 
+        //private readonly IUserService _userService;
+
+        //public MainWindow()
+        //{
+        //    InitializeComponent();
+        //    _context = new AppDbContext();
+        //    _userService = new UserService(_context);
+        //}
+
 
 
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            menuViewModel.LoadItems();  
+
+
+            MessageBox.Show("Відкрито розділ 'Замовлення'");
+            menuViewModel.LoadItems();
+
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+
+            MessageBox.Show("Додано новий елемент меню");
             menuViewModel.AddItem();
+
+
+
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Відкрито розділ 'Користувачі'"); 
 
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
 
+            Application.Current.Shutdown();
         }
     }
 }
